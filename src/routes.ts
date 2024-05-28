be9fe2a101071d83d4cfede6e80adff45c1e42e5
João Pedro Chaves <joaopedroaschaves@gmail.com>
2024-05-28 01:35:36 -0300

    feat: implementacao da funcionalidade de atualizar noticia

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { makeCreateNewsController } from "./api/controllers/createNews";
 import { adaptExpressRoute } from "./api/adapters/express-router";
 import { makeListNewsController } from "./api/controllers/listNews";
+import { makeUpdateNewsController } from "./api/controllers/updateNews";
 
 const routes = Router()
 
 routes.post('/create', adaptExpressRoute(makeCreateNewsController()))
 routes.get('/list', adaptExpressRoute(makeListNewsController()))
+routes.put('/update', adaptExpressRoute(makeUpdateNewsController()))
 
 export default routes;
